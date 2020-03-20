b0652bb0f862e9b5eb1c1ea5c8c358916e0d8c77
Hunter Lester <hlester@stubhub.com>
2020-03-20 14:43:52 -0700

    Odd even jump - iterative - poor time complexity

--- src/lib.rs
+++ src/lib.rs
@@ -19,9 +19,84 @@ pub fn unique_email_addresses(emails: Vec<String>) -> u32 {
     unique_addresses.len() as u32
 }
 
+// Time complexity is too great
+pub fn odd_even_jump_iterative(number_array: Vec<i16>) -> u16 {
+    let mut count = 1;
+    for i in 0..number_array.len() - 1 {
+        let mut index = i;
+        let mut jump = 1;
+        loop {
+            let is_odd_jump = jump % 2 != 0;
+            if is_odd_jump {
+                let nums_ge_index: Vec<&i16> = number_array[index + 1..].iter().filter(|n| n >= &&number_array[index]).collect();
+                if let Some(min_value) = nums_ge_index.iter().min() {
+                    if let Some(jump_index) = number_array.iter().enumerate().position(|(i, &element)| element == **min_value && i > index) {
+                        index = jump_index;
+                        jump += 1;
+                    }
+                } else {
+                    break;
+                }
+            } else {
+                let nums_le_index: Vec<&i16> = number_array[index + 1..].iter().filter(|n| n <= &&number_array[index]).collect();
+                if let Some(max_value) = nums_le_index.iter().max() {
+                    if let Some(jump_index) = number_array.iter().enumerate().position(|(i, &element)| element == **max_value && i > index) {
+                        index = jump_index;
+                        jump += 1;
+                    }
+                } else {
+                    break;
+                }
+            }
+            if index == number_array.len() - 1 {
+                count += 1;
+                break;
+            }
+        }
+    }
+    count
+}
+
+// pub fn odd_even_jump(number_array: Vec<i16>) -> u16 {
+//     let mut count = 1;
+//     for i in 0..number_array.len() - 1 {
+//         let mut index = i;
+//         let mut jump = 1;
+//         loop {
+//             let is_odd_jump = jump % 2 != 0;
+//             if is_odd_jump {
+//                 let nums_ge_index: Vec<&i16> = number_array[index + 1..].iter().filter(|n| n >= &&number_array[index]).collect();
+//                 if let Some(min_value) = nums_ge_index.iter().min() {
+//                     if let Some(jump_index) = number_array.iter().enumerate().position(|(i, &element)| element == **min_value && i > index) {
+//                         index = jump_index;
+//                         jump += 1;
+//                     }
+//                 } else {
+//                     break;
+//                 }
+//             } else {
+//                 let nums_le_index: Vec<&i16> = number_array[index + 1..].iter().filter(|n| n <= &&number_array[index]).collect();
+//                 if let Some(max_value) = nums_le_index.iter().max() {
+//                     if let Some(jump_index) = number_array.iter().enumerate().position(|(i, &element)| element == **max_value && i > index) {
+//                         index = jump_index;
+//                         jump += 1;
+//                     }
+//                 } else {
+//                     break;
+//                 }
+//             }
+//             if index == number_array.len() - 1 {
+//                 count += 1;
+//                 break;
+//             }
+//         }
+//     }
+//     count
+// }
+
 #[cfg(test)]
 mod tests {
-    use super::{unique_email_addresses};
+    use super::{unique_email_addresses, odd_even_jump_iterative};
     #[test]
     fn test_unique_email_addresses() {
         let emails = vec![
@@ -32,5 +107,23 @@ mod tests {
             String::from("vartanvartabed.kalustian@gmail.com"),
         ];
         assert_eq!(unique_email_addresses(emails), 1);
+    }
+
+    #[test]
+    fn test_odd_even_jump() {
+        let num_array = vec![10, 13, 12, 14, 15];
+        assert_eq!(odd_even_jump_iterative(num_array), 2);
+    }
+
+    #[test]
+    fn test_odd_even_jump_1() {
+        let num_array = vec![2, 3, 1, 1, 4];
+        assert_eq!(odd_even_jump_iterative(num_array), 3);
+    }
+
+    #[test]
+    fn test_odd_even_jump_2() {
+        let num_array = vec![5, 1, 3, 4, 2];
+        assert_eq!(odd_even_jump_iterative(num_array), 3);
     }
 }
