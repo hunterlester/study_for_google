03ea9cc3ff4a1dc7fceaa62623a0dd76fc002876
hunterlester <guilfordhunter@gmail.com>
2020-04-22 20:19:57 -0700

    Remove Nth Node From End of List

--- src/linked_lists.rs
+++ src/linked_lists.rs
@@ -72,9 +72,153 @@ pub fn add_two_numbers(mut l1: Option<Box<ListNode>>, mut l2: Option<Box<ListNod
     build_linked_list(sum_vec)
 }
 
+fn remove_nth_from_end(head: Option<Box<ListNode>>, n: i32) -> Option<Box<ListNode>> {
+    let mut list_node_pointers: Vec<Box<ListNode>> = Vec::new();
+    let mut current_node = head;
+    while let Some(boxed_node) = current_node {
+        list_node_pointers.push(boxed_node.clone());
+        current_node = boxed_node.next;
+    }
+
+    if n as usize == list_node_pointers.len() {
+        if n > 1 {
+            return Some(list_node_pointers[1].clone());
+        } else {
+            return None;
+        }
+    }
+
+    let mut pointer_1 = list_node_pointers.len() - n as usize - 1;
+    let mut pointer_2 = pointer_1 + 2;
+
+    if let Some(_node_1) = list_node_pointers.get(pointer_1) {
+        if let Some(_node_2) = list_node_pointers.get(pointer_2) {
+            list_node_pointers[pointer_1].next = Some(list_node_pointers[pointer_2].clone());
+        } else {
+            list_node_pointers[pointer_1].next = None;
+        }
+    }
+
+    while pointer_1 >= 1 {
+        list_node_pointers[pointer_1 - 1].next = Some(list_node_pointers[pointer_1].clone());
+        pointer_1 -= 1;
+    }
+
+    Some(list_node_pointers[0].clone())
+}
+
 #[cfg(test)]
 mod tests {
-    use super::{add_two_numbers, ListNode};
+    use super::{add_two_numbers, ListNode, remove_nth_from_end};
+
+    #[test]
+    fn test_remove_nth_from_end() {
+        let mut l1 = Some(Box::new(ListNode {
+            val: 1,
+            next: None, 
+        }));
+        let mut expected = Some(Box::new(ListNode {
+            val: 1,
+            next: None, 
+        }));
+        assert_eq!(remove_nth_from_end(l1, 0), expected);
+        l1 = Some(Box::new(ListNode {
+            val: 1,
+            next: None, 
+        }));
+        expected = None;
+        assert_eq!(remove_nth_from_end(l1, 1), expected);
+        l1 = Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode {
+                val: 2,
+                next: Some(Box::new(ListNode {
+                    val: 3,
+                    next: None,
+                })),
+            }))
+        }));
+        expected = Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode {
+                val: 2,
+                next: None,
+            }))
+        }));
+        assert_eq!(remove_nth_from_end(l1, 1), expected);
+        l1 = Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode {
+                val: 2,
+                next: Some(Box::new(ListNode {
+                    val: 3,
+                    next: None,
+                })),
+            }))
+        }));
+        expected = Some(Box::new(ListNode {
+            val: 2,
+            next: Some(Box::new(ListNode {
+                val: 3,
+                next: None,
+            }))
+        }));
+        assert_eq!(remove_nth_from_end(l1, 3), expected);
+        let mut l1 = Some(Box::new(ListNode {
+            val: 2,
+            next: Some(Box::new(ListNode {
+                val: 4,
+                next: None,
+            }))
+        }));
+        let mut expected = Some(Box::new(ListNode {
+            val: 4,
+            next: None, 
+        }));
+        assert_eq!(remove_nth_from_end(l1, 2), expected);
+        let mut l1 = Some(Box::new(ListNode {
+            val: 2,
+            next: Some(Box::new(ListNode {
+                val: 4,
+                next: None,
+            }))
+        }));
+        let mut expected = Some(Box::new(ListNode {
+            val: 2,
+            next: None, 
+        }));
+        assert_eq!(remove_nth_from_end(l1, 1), expected);
+        l1 = Some(Box::new(ListNode {
+            val: 2,
+            next: Some(Box::new(ListNode {
+                val: 4,
+                next: Some(Box::new(ListNode {
+                    val: 3,
+                    next: Some(Box::new(ListNode {
+                        val: 1,
+                        next: Some(Box::new(ListNode {
+                            val: 5,
+                            next: None,
+                        })),
+                    })),
+                }))
+            }))
+        }));
+        expected = Some(Box::new(ListNode {
+            val: 2,
+            next: Some(Box::new(ListNode {
+                val: 4,
+                next: Some(Box::new(ListNode {
+                    val: 3,
+                    next: Some(Box::new(ListNode {
+                        val: 5,
+                        next: None,
+                    })),
+                }))
+            }))
+        }));
+        assert_eq!(remove_nth_from_end(l1, 2), expected);
+    }
 
     #[test]
     fn test_add_two_numbers() {
